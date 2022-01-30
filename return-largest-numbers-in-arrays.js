cbf54247954442c59c351f56025b11a50d019aaa
Ola793 <67291449+Ola793@users.noreply.github.com>
2022-01-30 18:26:17 +0200

    Add files via upload
    Return an array consisting of the largest number from each provided sub-array.

---
+++ return-largest-numbers-in-arrays.js
@@ -0,0 +1,13 @@
+function largestOfFour(arr) {
+  let arrOfMax = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      let subArr = arr[i];
+      subArr.sort((a, b) => b - a);
+      arrOfMax.push(subArr[0]);
+    }
+  }
+  return arrOfMax;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
