19f8073af3d9116119ffcdef4d1e77d84cbf163a
Olha Teplova <67291449+Ola793@users.noreply.github.com>
2022-07-18 00:54:07 +0300

    Update confirm-the-ending.js

--- confirm-the-ending.js
+++ confirm-the-ending.js
@@ -1,7 +1,7 @@
 // Check if a string (first argument, str) ends with the given target string (second argument, target).
 
 function confirmEnding(str, target) {
-  return str.substring(str.length - target.length, str.length) === target ? true : false;
+  return str.substring(str.length - target.length, str.length) === target;
 }
 
 confirmEnding("Bastian", "n");
